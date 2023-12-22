c4a71d62260e5bddc55c4e65d76e32b5b4b9e3c7
Pildomfl <151389400+Pildomfl@users.noreply.github.com>
2023-12-22 20:34:20 +0100

    Fixed javascript structure

--- src/index.js
+++ src/index.js
@@ -12,17 +12,19 @@ function generateYear(event) {
   event.preventDefault();
 
   let instructionsInput = document.querySelector("#user-instructions");
+  let yearInput = document.querySelector("#user-year");
   let apiKey = "f060a71819e2tbf51f08f683o95b4c96";
-  let prompt = `User instructions:Generate an event that happened in ${instructionsInput.value}`;
+  let prompt = `User instructions: Generate an event that happened in that year ${yearInput.value}`;
   let context =
-    "You are an history expert and love to write about history. your mission is to generate a historical event of 2 lines maximum in basic HTML. Make sure to follow the user instructions";
-  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    "You are a history expert and love to write about history. Your mission is to generate a historical event of maximum 2 lines in basic HTML. Make sure to follow the user instructions";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(
+    prompt
+  )}&context=${context}&key=${apiKey}`;
 
   console.log("Generating answer");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
   axios.get(apiURL).then(displayAnswer);
-
-  let yearFormElement = document.querySelector("#year-generator-form");
-  yearFormElement.addEventListener("submit", generateYear);
 }
+let yearFormElement = document.querySelector("#year-generator-form");
+yearFormElement.addEventListener("submit", generateYear);
