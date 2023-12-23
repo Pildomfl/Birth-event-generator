04ff21bf7b220a4b0be5a0278725a6eccb2e3c39
Pildomfl <151389400+Pildomfl@users.noreply.github.com>
2023-12-23 13:24:49 +0100

    final touch and project ready

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function generateYear(event) {
   let apiKey = "f060a71819e2tbf51f08f683o95b4c96";
   let prompt = `User instructions: Generate an event that happened in that year ${yearInput.value}`;
   let context =
-    "You are a history expert and love to write about history. Your mission is to generate a historical event of maximum 2 lines in basic HTML. Make sure to follow the user instructions and always start the answer with the words In that year. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the answer";
+    "You are a history expert and love to write about history. Your mission is to generate a historical event of maximum 2 lines in basic HTML. Make sure to follow the user instructions and always start the answer with the words In that year. Sign the answer with 'SheCodes AI' inside a <strong> element at the end of the answer";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(
     prompt
   )}&context=${context}&key=${apiKey}`;
